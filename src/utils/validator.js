2bd0087cadac9f33fa4241551cb024371dfdc8de
Himanshu Kumar <himansu9508@gmail.com>
2026-03-19 13:48:26 +0530

    editUser && password validation

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -9,5 +9,24 @@ if(!firstName || !lastName){
 }else if (!validator.isStrongPassword(password)){
       throw new Error("Please enter a strong password")
 }
+};
+
+const validateUserEdit = (req)=>{
+      const allowedEditFile = ["firstName" , "lastName" ,"age" ,"skills" ,"gender" , "photoUrl" , "about"];
+      const isAllowedUpdate = Object.keys(req.body).every(key => allowedEditFile.includes(key));
+     return isAllowedUpdate;
+};
+
+
+const validateEditPassword = (req)=>{
+       const { newPassword , verifiedNewPassword} = req.body;
+       if(newPassword !== verifiedNewPassword ){
+            throw new Error("New and retype  passwords are not same");
+       }
+       if(!validator.isStrongPassword(newPassword)){
+            throw new Error("Please use a Strong password !")
+       }
+       return true;
+
 }
-module.exports = {validateSignupUser};
+module.exports = {validateSignupUser , validateUserEdit , validateEditPassword};
