911906d95f7a0a3dff36f249efcb72930309f3a0
Himanshu Kumar <himansu9508@gmail.com>
2026-03-19 13:46:35 +0530

    API : sendConnectionRequest

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const {userAuth} = require("../middleware/auth");
+const ConnectionRequest = require("../model/connectionRequest");
 const requestRouter = express.Router();
 
-requestRouter.post("request" , userAuth ,async  (req, res)=>{
+requestRouter.post("/request/send/:status/:userId" , userAuth ,async (req, res)=>{
       try{
-      const user =await  req.user;
-      res.send(user.firstName + "Sending the connection request");
+            const fromUserId = req.user._id;
+            const toUserId = req.params.userId;
+            const status = req.params.status;
+            const allowedStatus = ["interested" , "ignored"];
+            if(!allowedStatus.includes(status)){
+                  throw new Error("invalid status type : " + status);
+            }
+            const connectionRequest = new ConnectionRequest({
+                  fromUserId ,
+                  toUserId,
+                  status
+            })
+           const data  =  await connectionRequest.save();
+            res.json({
+                  message:`Connection request sent successfuly`,
+                  data,
+            }
+            )
+            
       }catch(er){
             res.status(400).send("Error : " + er.message);
       }
