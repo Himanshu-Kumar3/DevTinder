441981ee42e3d128d6e4fc5e2027926534f8e5bf
Himanshu Kumar <himansu9508@gmail.com>
2026-03-20 11:29:58 +0530

    added more validation in connection request

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const {userAuth} = require("../middleware/auth");
 const ConnectionRequest = require("../model/connectionRequest");
+const User = require("../model/user");
 const requestRouter = express.Router();
+
 
 requestRouter.post("/request/send/:status/:userId" , userAuth ,async (req, res)=>{
       try{
@@ -16,7 +18,25 @@ requestRouter.post("/request/send/:status/:userId" , userAuth ,async (req, res)=
                   fromUserId ,
                   toUserId,
                   status
-            })
+            });
+
+            // if the toUser is not exists:
+           const toUser = await User.findById(toUserId);
+           if(!toUser){
+            res.status(404).send({message:"User not exists!!"})
+           }
+
+      //      If p1 -> p2 already exists or p2 -> p1 
+           const existingConnectionRequest = await ConnectionRequest.findOne({
+            $or:[
+                  {fromUserId , toUserId},
+                  {fromUserId:toUserId , toUserId: fromUserId}
+            ]
+           });
+
+           if(existingConnectionRequest){
+            res.status(400).json({message:"Can't send connection request !!"})
+           }
            const data  =  await connectionRequest.save();
             res.json({
                   message:`Connection request sent successfuly`,
